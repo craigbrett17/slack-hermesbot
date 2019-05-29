c79abbfa300743cdd254c3c9856bfd9748403abc
Craig Brett <craig.brett@argusmedia.com>
2019-05-29 14:03:57 +0100

    Initial commit of C# code

---
+++ HermesBot.Service/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace HermesBot.Service
+{
+    public class Service
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Launching service... Looking for streams to check.");
+
+
+        }
+    }
+}
